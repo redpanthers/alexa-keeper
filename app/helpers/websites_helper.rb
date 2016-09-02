a2c789f5a006fd60dd8ae74b263952261a611b24
sameesh-s <sameesh.fisat@gmail.com>
2016-09-02 16:01:28 +0530

    Data points for High Chart

--- app/helpers/websites_helper.rb
+++ app/helpers/websites_helper.rb
@@ -1,2 +1,9 @@
 module WebsitesHelper
+  def self.createJSON(alexaRank)
+    series_data = []
+    alexaRank.each do |site|
+      series_data << { name: site.created_at.to_s, data: [site.rank.to_i] } 
+    end
+    alexaRank
+  end
 end
