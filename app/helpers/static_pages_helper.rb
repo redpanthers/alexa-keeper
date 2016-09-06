a7040894a14e4f44167e67760356fcb2f7218734
harisr92 <harikrishnansr92@live.com>
2016-09-06 14:37:07 +0530

    fix categorise data in graph

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -4,12 +4,15 @@ module StaticPagesHelper
     website.each do |site|
       demo_series = {}
       ranks = []
-      dates  = []
+      data  = []
+      category = []
       site.alexaranks.each do |rank|
-        ranks << rank.rank.to_i
-        dates << rank.created_at.to_s[0..10]
+        create_data = {}
+        category << rank.created_at.to_s[0..10]
+        create_data = {:name => rank.created_at.to_s[0..10], :y => rank.rank.to_i}
+        data.push(create_data)
       end
-      demo_series = { :name => site.url, :data => ranks, :date => dates }
+      demo_series = { :name => site.url, :data => data, :date => category }
       series.push(demo_series)
     end
     series = series.map{ |o| Hash[o.each_pair.to_a]}
