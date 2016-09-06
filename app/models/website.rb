8ac3442f7741f1fd49951e68c981ca53e4c757dc
harisr92 <harikrishnansr92@live.com>
2016-09-06 14:01:38 +0530

    Fix enter url fordifferent users

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,7 +1,7 @@
 class Website < ApplicationRecord
   belongs_to :user
   has_many :alexaranks, dependent: :destroy
-  validates :url, presence: true, uniqueness: true
+  validates :url, presence: true
   validates :user_id, presence: true
   validates_uniqueness_of :url, :scope => :user_id
 end
