ce6bae014a1b9c0155fc3c9bd5d1af3d4f58ceaf
Harisankar P S <mailme@hsps.in>
2016-09-08 11:06:09 +0530

    Move alexa rank fetch to a single location

--- app/jobs/website_add_job.rb
+++ app/jobs/website_add_job.rb
@@ -1,17 +1,9 @@
-require 'nokogiri'
-require 'open-uri'
 
 class WebsiteAddJob
   include SuckerPunch::Job
   workers 4
 
   def perform(website)
-    str  = 'http://www.alexa.com/siteinfo/'+website.url.to_s
-    doc  = Nokogiri::HTML(open(str))
-    rank = doc.css('strong.metrics-data.align-vmiddle')
-              .inner_text.to_s.gsub(',', '')
-              .split
-              .first
-    website.alexaranks.create!(rank:rank.to_i)
+    website.fetch_alexa_rank_and_update!
   end
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -4,4 +4,14 @@ class Website < ApplicationRecord
   validates :url, presence: true
   validates :user_id, presence: true
   validates_uniqueness_of :url, :scope => :user_id
+
+  def fetch_alexa_rank_and_update!
+    begin
+      rank = Alexarank.fetch_rank domain: url.to_s
+      self.alexaranks.create(rank: rank)
+    rescue Exception => e
+      Rails.logger.info "[AFE] #{url.to_s} rank fetch failed."
+      Rails.logger.info "[AFE] #{e.message}"
+    end
+  end
 end
