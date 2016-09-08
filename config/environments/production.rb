3549344068c3a23f1bc54691fef8a8f0a237d14b
sameesh-s <sameesh.fisat@gmail.com>
2016-09-08 18:00:58 +0530

    sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   #config for sendgrid mail
-  config.active_record.default_url_options = { host:'https://alexa-keeper.herokuapp.com/'}
+  config.active_mailer.default_url_options = { host:'https://alexa-keeper.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
