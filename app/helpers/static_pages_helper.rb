d5b1fef26fd2a7c1431ec91f7afccc992d7c0bca
harisr92 <harikrishnansr92@live.com>
2016-09-08 16:30:10 +0530

    Fetch last 10 days ranks in multiple domains

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,20 +1,20 @@
 module StaticPagesHelper
-  def self.createJSON(website)
-    series = []
-    website.each do |site|
-      demo_series = {}
-      ranks = []
-      data  = []
-      category = []
-      site.alexaranks.each do |rank|
-        create_data = {}
-        category << rank.created_at.to_s[0..10]
-        create_data = {:name => rank.created_at.to_s[0..10], :y => rank.rank.to_i}
-        data.push(create_data)
-      end
-      demo_series = { :name => site.url, :data => data, :date => category.uniq }
-      series.push(demo_series)
+    def self.createJSON(website)
+        series = []
+        website.each do |site|
+            demo_series = {}
+            ranks = []
+            data  = []
+            category = []
+            site.fetch_last_10_days_rank.each do |rank|
+                create_data = {}
+                category << rank.created_at.strftime('%Y-%m-%d')
+                create_data = { name: rank.created_at.strftime('%Y-%m-%d'), y: rank.rank.to_i }
+                data.push(create_data)
+            end
+            demo_series = { name: site.url, data: data, date: category.uniq }
+            series.push(demo_series)
+        end
+        series = series.map { |o| Hash[o.each_pair.to_a] }
     end
-    series = series.map{ |o| Hash[o.each_pair.to_a]}
-  end
 end
