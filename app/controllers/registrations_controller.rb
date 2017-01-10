0fc249d7359b6f15c263288e599fbb373d5d713a
NimmyVipin <nimmyvipin25@gmail.com>
2017-01-10 13:12:12 +0530

    innerfooter and feedback form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
   def after_sign_up_path_for(resource)
     @user = current_user.email
-     flash[:notice] = "A mail successfully sent"
+    
     UserMailer.welcome_email(@user).deliver_now
       after_sign_in_path_for(resource)
   end
