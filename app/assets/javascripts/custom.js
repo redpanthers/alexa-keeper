a7dcd333aef162b7852ef6b2ed7c2865ffc224df
NimmyVipin <nimmyvipin25@gmail.com>
2017-03-08 11:26:52 +0530

    display graph list wise

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-   
-
   var slider = ["ALERT","SMART","COMPETITIVE"];
   $("#animate").addClass('animated infinite fadeInDown');
   $("#animate").text(slider[0]);
@@ -16,9 +14,9 @@ $(document).ready(function() {
     $.getJSON('/static_pages/show', function(data) {
         console.log(data)
         $('#graph').highcharts({
-	    chart: {
-		    zoomType: 'x'
-	    },
+      chart: {
+        zoomType: 'x'
+      },
             title: {
                 text: 'Alexa Keeper',
                 x: -20 //center
@@ -64,6 +62,7 @@ $(document).ready(function() {
             },
             series: data
         });
+   render_graph(data);
     })
   }
 
@@ -110,19 +109,83 @@ $( "#invite" ).click(function() {
 })
 
 function openNav() {
-	    var e = document.getElementById("mySidenav");
-	    if (e.style.width == '250px')
-	    {
-	        e.style.width = '0px';
-	    }
-	    else 
-	    {
-	        e.style.width = '250px';
-	    }
-	}
+      var e = document.getElementById("mySidenav");
+      if (e.style.width == '250px')
+      {
+          e.style.width = '0px';
+      }
+      else 
+      {
+          e.style.width = '250px';
+      }
+  }
 
-	function closeNav() {
-	    document.getElementById("mySidenav").style.width = "0";
-	}
+  function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+  }
 
   
+function showListWiseGraph(id, index){
+     var isExpanded = $("#collection" + index).attr("aria-expanded");
+     if(isExpanded === 'true'){
+         return;
+     }
+     
+     $.getJSON('/static_pages/show', { collection_id: id}, function(data) {
+         render_graph(data);
+     })    
+ }
+
+
+
+function render_graph(data){
+     $('#graph').highcharts({
+         chart: {
+          zoomType: 'x'
+         },
+         title: {
+             text: 'Alexa Keeper',
+             x: -20 //center
+         },
+         subtitle: {
+             text: 'Source: alexa.com',
+             x: -20
+         },
+         xAxis: {
+             categories: data[0]['date'],
+             min: 0
+         },
+         yAxis: {
+             reversed : 'true',
+             title: {
+                 text: 'Ranks'
+             },
+             labels: {
+                 formatter: function() {
+                     var rate = this.value;
+                     if (rate > 999999) {
+                         rate = Math.round((rate / 1000000) * 100) / 100 + " M";
+                     } else if (rate > 999 && rate < 1000000) {
+                         rate = Math.round((rate / 1000) * 100) / 100 + " k";
+                     }
+                     return rate;
+                 }
+             },
+             plotLines: [{
+                 value: 0,
+                 width: 1,
+                 color: '#808080'
+             }]
+         },
+         tooltip: {
+             valueSuffix: ''
+         },
+         legend: {
+             layout: 'vertical',
+             align: 'right',
+             verticalAlign: 'middle',
+             borderWidth: 0
+         },
+         series: data
+     });
+ }
