1e646e0d89ef3ade6ba459afed18af68fe40bde9
NimmyVipin <nimmyvipin25@gmail.com>
2017-03-29 12:15:53 +0530

    remove token from user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328152633) do
+ActiveRecord::Schema.define(version: 20170329064201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,13 +111,11 @@ ActiveRecord::Schema.define(version: 20170328152633) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
-    t.string   "token"
     t.integer  "list_number"
     t.integer  "site_number"
     t.string   "sites",                  default: [],                 array: true
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
   create_table "websites", force: :cascade do |t|
