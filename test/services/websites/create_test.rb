951658478dc12c721eceae1abe57a196522edea1
NimmyVipin <nimmyvipin25@gmail.com>
2017-04-11 18:31:20 +0530

    Modify test file to fix failures

--- test/services/websites/create_test.rb
+++ test/services/websites/create_test.rb
@@ -6,6 +6,21 @@ module Websites
       user = users(:hari)
       collection = user.collections.create!(name: 'My Collection')
 
+      params = { website: { url: 'http://twitter.com', collection_id: collection.id } }
+
+      Websites::Create.call(params, user)
+
+      user.reload
+      assert_equal 1, user.site_number, 'site number'
+      assert_equal 1, Website.count
+      assert_equal ['http://twitter.com'], user.websites.pluck(:url)
+      assert_equal ['http://twitter.com'], collection.websites.pluck(:url)
+    end
+
+    test 'adding a new website without http prefix' do
+      user = users(:hari)
+      collection = user.collections.create!(name: 'My Collection')
+
       params = { website: { url: 'twitter.com', collection_id: collection.id } }
 
       Websites::Create.call(params, user)
@@ -13,25 +28,25 @@ module Websites
       user.reload
       assert_equal 1, user.site_number, 'site number'
       assert_equal 1, Website.count
-      assert_equal ['twitter.com'], user.websites.pluck(:url)
-      assert_equal ['twitter.com'], collection.websites.pluck(:url)
+      assert_equal ['http://twitter.com'], user.websites.pluck(:url)
+      assert_equal ['http://twitter.com'], collection.websites.pluck(:url)
     end
 
     test 'does not create duplicate website' do
       user = users(:hari)
       collection = user.collections.create!(name: 'My Collection')
-      Website.create!(url: 'twitter.com')
+      Website.create!(url: 'http://twitter.com')
 
-      params = { website: { url: 'twitter.com', collection_id: collection.id } }
+      params = { website: { url: 'http://twitter.com', collection_id: collection.id } }
 
       assert_no_difference 'Website.count' do
         Websites::Create.call(params, user)
       end
 
       user.reload
-      assert_equal 1, user.site_number
-      assert_equal ['twitter.com'], user.websites.pluck(:url)
-      assert_equal ['twitter.com'], collection.websites.pluck(:url)
+      assert_equal 1, user.websites.count
+      assert_equal ['http://twitter.com'], user.websites.pluck(:url)
+      assert_equal ['http://twitter.com'], collection.websites.pluck(:url)
     end
   end
 end
